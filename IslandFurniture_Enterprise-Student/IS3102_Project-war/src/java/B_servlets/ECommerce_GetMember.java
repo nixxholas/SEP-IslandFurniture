27ed20a5354b78063238b30e51103b9f4d82ffaa
Gabriel <gabriel@Gabriels-MacBook-Pro.local>
2016-12-14 15:40:14 +0800

    Enterprise: war: B_servlets: ECommerce_GetMember
    ECommerce_GetMember allows us to properly retrieve the user's data in order to authenticate and push the user information into the session and as a cookie.
    
    Signed-off-by: Gabriel <gabriel@Gabriels-MacBook-Pro.local>

---
+++ IslandFurniture_Enterprise-Student/IS3102_Project-war/src/java/B_servlets/ECommerce_GetMember.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package B_servlets;
+
+import HelperClasses.Member;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.persistence.NoResultException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author nixho
+ */
+@WebServlet(name = "ECommerce_GetMember", urlPatterns = {"/ECommerce_GetMember"})
+public class ECommerce_GetMember extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession();
+        PrintWriter out = response.getWriter();
+                  
+        try {
+            Member member = retrieveMember((String) session.getAttribute("memberEmail"));
+            
+            // Member Details for Session
+            session.setAttribute("member", member);
+
+            session.setAttribute("memberName", member.getName());
+            
+            //  RequestDispatcher rd = request.getRequestDispatcher("/IS3102_Project-war/B/SG/memberProfile.jsp");
+            //  rd.forward(request, response);
+
+            response.sendRedirect("/IS3102_Project-war/B/SG/memberProfile.jsp");
+        } catch (NoResultException ex) {
+            out.println("Failed to find member.");
+        } catch (Exception ex) {
+            out.println("\nServer failed to getMemberByEmail:\n" + ex);
+            ex.printStackTrace();
+        }
+    }
+    
+    public Member retrieveMember(String email) {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client
+                .target("http://localhost:8080/IS3102_WebService-Student/webresources/entity.memberentity").path("getmember")
+                .queryParam("email", email);
+        Invocation.Builder invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+        Response response = invocationBuilder.get();
+        System.out.println("status: " + response.getStatus());
+
+        if (response.getStatus() != 200) {
+            return null;
+        }
+
+        Member member = response.readEntity(Member.class);
+        return member;
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
